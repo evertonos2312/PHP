21bf2edecf3509867035d9bcacc1d20603c399d8
Everton Silva <57024315+evertonos2312@users.noreply.github.com>
2020-07-19 20:11:26 -0300

    Introdução PHP, array, condicionais e laço de repetição

--- index.php
+++ index.php
@@ -6,6 +6,33 @@ $categorias[] = 'adolescente';
 $categorias[] = 'adulto';
 $categorias[] = 'idoso';
 
-print_r($categorias);
+//print_r($categorias);
+
+$nome = 'Everton';
+$idade = 18;
+
+//var_dump($nome);
+//var_dump($idade);
+
+if($idade >= 6 && $idade <=12)
+{
+    for($i = 0; $i < sizeof($categorias); $i++)
+    {
+        if($categorias[$i] == 'infantil')
+            echo "O nadador ".$nome. " tem ".$idade." anos e compete na categoria ".$categorias[$i];
+    }
+}
+else if($idade >= 13 && $idade <= 18)
+{
+    for($i = 0; $i < sizeof($categorias); $i++)
+    {
+        if($categorias[$i] == 'adolescente')
+            echo "O nadador ".$nome. " tem ".$idade." anos e compete na categoria ".$categorias[$i];
+    }
+}
+else
+{
+    echo "O nadador ".$nome. " tem ".$idade." anos e compete na categoria adulto";
+}
 
 
